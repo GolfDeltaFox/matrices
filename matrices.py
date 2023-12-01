6d1ac0e497de9cd9a7d2df3f1ce6d7fa244f40ea
Geoffroy Dubourg-Felonneau <nycgdf@gmail.com>
2023-11-30 22:00:00 -0800

    feat: add caching

--- matrices.py
+++ matrices.py
@@ -2,11 +2,21 @@ from flask import Flask, send_file
 from astro_weather_service import AstroWeatherService as Aws
 import os
 import random
+import flask
+import urllib
+from flask_caching import Cache
+
 app = Flask(__name__)
+cache = Cache(app, config={"CACHE_TYPE": "simple"})
+
 aws = Aws()
 
+def cache_key():
+    print(flask.request.url)
+    return flask.request.url
 
 @app.route('/matrices/astro/<longitude>/<latitude>/<zoom>')
+@cache.cached(timeout=1800, key_prefix=cache_key)
 def myapp(longitude, latitude, zoom):
     """
     Returns a random image directly through send_file
